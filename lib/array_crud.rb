22b9abb57e05e0b4d251dd4fd5ab203cc3c2bafb
erin dougher <ejdougher@gmail.com>
2020-11-21 04:38:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,8 @@ retrieve_element[2]
 end
 
 def retrieve_first_element_from_array(array)
-
+retrieve_first_element = ["wow", "I", "am", "really", "learning", "arrays!"]
+retrieve_first_element[0]
 end
 
 def retrieve_last_element_from_array(array)
